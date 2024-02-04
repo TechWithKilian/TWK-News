abc73e1e12027f74ce9b94367a26b832d770517f
N3v1 <n3v1010@gmail.com>
2024-02-04 16:21:27 +0100

    Initial Commit

---
+++ TWK News/TWK_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TWK_NewsApp.swift
+//  TWK News
+//
+//  Created by Nevio Hirani on 04.02.24.
+//
+
+import SwiftUI
+
+@main
+struct TWK_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
