d8ab99725d0f54770cd21099ec56af7851739049
N3v1 <n3v1010@gmail.com>
2024-02-05 19:57:52 +0100

    [Chore]: Set up swiftlint and resolve all issues

--- TWK News/App/TWKAppDelegate.swift
+++ TWK News/App/TWKAppDelegate.swift
@@ -1,33 +1,33 @@
-//===-------------------------------TechWithKilian-AppDelegate-------------------------------===//
-//                                                                                              //
-//  TWKAppDelegate.swift                                                                        //
-//  TWKNews Core                                                                                //
-//                                                                                              //
-//  Copyright (c) 2024 TechWithKilian.                                                          //
-//                                                                                              //
-//  Licensed under the Apache License, Version 2.0 (the "License");                             //
-//  you may not use this file except in compliance with the License.                            //
-//  You may obtain a copy of the License at                                                     //
-//                                                                                              //
-//       http://www.apache.org/licenses/LICENSE-2.0                                             //
-//                                                                                              //
-//  Unless required by applicable law or agreed to in writing, software                         //
-//  distributed under the License is distributed on an "AS IS" BASIS,                           //
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                    //
-//  See the License for the specific language governing permissions and                         //
-//  limitations under the License.                                                              //
-//                                                                                              //
-//  YouTube: https://www.youtube.com/@TechWithKilian                                            //
-//  See https://github.com/TechWithKilian/TWKNews/blob/main/LICENSE for license information     //
-//  See https://github.com/TechWithKilian/TWK-News/graphs/contributors for the list of          //
-//  all Contributors.                                                                           //
-//                                                                                              //
-//===----------------------------------------------------------------------------------------===//
-//                                                                                              //
-// This file contains settings and implementations for TWK News Client and                      //
-// TWK News's DB.                                                                               //
-//                                                                                              //
-//===----------------------------------------------------------------------------------------===//
+// ===-------------------------------TechWithKilian-AppDelegate-------------------------------=== //
+//                                                                                                //
+//  TWKAppDelegate.swift                                                                          //
+//  TWKNews Core                                                                                  //
+//                                                                                                //
+//  Copyright (c) 2024 TechWithKilian.                                                            //
+//                                                                                                //
+//  Licensed under the Apache License, Version 2.0 (the "License");                               //
+//  you may not use this file except in compliance with the License.                              //
+//  You may obtain a copy of the License at                                                       //
+//                                                                                                //
+//       http://www.apache.org/licenses/LICENSE-2.0                                               //
+//                                                                                                //
+//  Unless required by applicable law or agreed to in writing, software                           //
+//  distributed under the License is distributed on an "AS IS" BASIS,                             //
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                      //
+//  See the License for the specific language governing permissions and                           //
+//  limitations under the License.                                                                //
+//                                                                                                //
+//  YouTube: https://www.youtube.com/@TechWithKilian                                              //
+//  See https://github.com/TechWithKilian/TWKNews/blob/main/LICENSE for license information       //
+//  See https://github.com/TechWithKilian/TWK-News/graphs/contributors for the list of            //
+//  all Contributors.                                                                             //
+//                                                                                                //
+// ===----------------------------------------------------------------------------------------=== //
+//                                                                                                //
+// This file contains settings and implementations for TWK News Client and                        //
+// TWK News's DB.                                                                                 //
+//                                                                                                //
+// ===----------------------------------------------------------------------------------------=== //
 
 #if os(iOS)
 import UIKit
@@ -54,7 +54,7 @@ class TWKAppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterD
         let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
         UNUserNotificationCenter.current().requestAuthorization(
             options: authOptions,
-            completionHandler: { granted, _ in
+            completionHandler: { _,_  in /*granted, _ in*/
                 
             }
         )
--- TWK News/App/TWK_NewsApp.swift
+++ TWK News/App/TWK_NewsApp.swift
@@ -1,33 +1,33 @@
-//===-------------------------------TechWithKilian-Entry-Point-------------------------------===//
-//                                                                                              //
-//  TWKNewsApp.swift                                                                            //
-//  TWKNews Core                                                                                //
-//                                                                                              //
-//  Copyright (c) 2024 TechWithKilian.                                                          //
-//                                                                                              //
-//  Licensed under the Apache License, Version 2.0 (the "License");                             //
-//  you may not use this file except in compliance with the License.                            //
-//  You may obtain a copy of the License at                                                     //
-//                                                                                              //
-//       http://www.apache.org/licenses/LICENSE-2.0                                             //
-//                                                                                              //
-//  Unless required by applicable law or agreed to in writing, software                         //
-//  distributed under the License is distributed on an "AS IS" BASIS,                           //
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                    //
-//  See the License for the specific language governing permissions and                         //
-//  limitations under the License.                                                              //
-//                                                                                              //
-//  YouTube: https://www.youtube.com/@TechWithKilian                                            //
-//  See https://github.com/TechWithKilian/TWKNews/blob/main/LICENSE for license information     //
-//  See https://github.com/TechWithKilian/TWK-News/graphs/contributors for the list of          //
-//  all Contributors.                                                                           //
-//                                                                                              //
-//===----------------------------------------------------------------------------------------===//
-//                                                                                              //
-// This file declares the app's main entry point and contains configurations                    //
-// for TWK News's Views                                                                         //
-//                                                                                              //
-//===----------------------------------------------------------------------------------------===//
+// ===-------------------------------TechWithKilian-Entry-Point------------------------------=== //
+//                                                                                               //
+//  TWKNewsApp.swift                                                                             //
+//  TWKNews Core                                                                                 //
+//                                                                                               //
+//  Copyright (c) 2024 TechWithKilian.                                                           //
+//                                                                                               //
+//  Licensed under the Apache License, Version 2.0 (the "License");                              //
+//  you may not use this file except in compliance with the License.                             //
+//  You may obtain a copy of the License at                                                      //
+//                                                                                               //
+//       http://www.apache.org/licenses/LICENSE-2.0                                              //
+//                                                                                               //
+//  Unless required by applicable law or agreed to in writing, software                          //
+//  distributed under the License is distributed on an "AS IS" BASIS,                            //
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                     //
+//  See the License for the specific language governing permissions and                          //
+//  limitations under the License.                                                               //
+//                                                                                               //
+//  YouTube: https://www.youtube.com/@TechWithKilian                                             //
+//  See https://github.com/TechWithKilian/TWKNews/blob/main/LICENSE for license information      //
+//  See https://github.com/TechWithKilian/TWK-News/graphs/contributors for the list of           //
+//  all Contributors.                                                                            //
+//                                                                                               //
+// ===---------------------------------------------------------------------------------------=== //
+//                                                                                               //
+// This file declares the app's main entry point and contains configurations                     //
+// for TWK News's Views                                                                          //
+//                                                                                               //
+// ===---------------------------------------------------------------------------------------=== //
 
 import SwiftUI
 import TipKit
